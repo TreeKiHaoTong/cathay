79be26b27c8c77bdf0134586dee5624fae3ff496
TreeKiHaoTong <chengyenk@gmail.com>
2025-08-03 01:04:52 +0800

    Update API testing framework with new modules and clean up documentation

--- configs.py
+++ configs.py
@@ -2,50 +2,25 @@ PRODUCT = {
     'api_test' : 'api_test/product/cathay_products.postman_collection.json' 
 }
 
-# CATEGORY = {
-#     'api_test' : 'api_test/category/cathay_category.postman_collection.json'
-# }
+CATEGORIES = {
+    'api_test' : 'api_test/categories/cathay_categories.postman_collection.json'
+}
 
-# BRAND = {
-#     'api_test' : 'api_test/brand/cathay_brand.postman_collection.json'
-# }
+BRAND = {
+    'api_test' : 'api_test/brand/cathay_brands.postman_collection.json'
+}
 
-# GRAPH = {
-#     'api_test' : 'api_test/graph/CORE_graph.postman_collection.json'
-# }
-
-# ORGANIZATION = {
-#     'api_test' : 'api_test/organization/CORE_organization.postman_collection.json'
-# }
-
-# USER_ADMIN = {
-#     'api_test' : 'api_test/user/CORE_user(admin).postman_collection.json'
-# }
-
-# USER_MANAGER = {
-#     'api_test' : 'api_test/user/CORE_user(manager).postman_collection.json'
-# }
-
-# USER_USER = {
-#     'api_test' : 'api_test/user/CORE_user(user).postman_collection.json'
-# }
-
-# CASE = {
-#     'api_test' : 'api_test/case/CORE_case.postman_collection.json'
-# }
-
-# TRACK = {
-#     'api_test' : 'api_test/track/CORE_track.postman_collection.json'
-# }
-
-# CASE_DOC = {
-#     'api_test' : 'api_test/case_doc/CORE_case-doc.postman_collection.json'
-# }
+MESSAGE = {
+    'api_test' : 'api_test/message/cathay_messages.postman_collection.json'
+}
 
 # 併發執行的測試
-MODULE = {'product': PRODUCT}
+MODULE = {
+    'product': PRODUCT,
+    'categories': CATEGORIES,
+    'brand': BRAND,
+    'message': MESSAGE
+}
 
 # 不併發執行的測試，會在併發測試結束後，依序執行
 MODULE_NON_CONCURRENT = {}
-
-# MODULE_SCHEDULE = {'case_doc': CASE_DOC}
